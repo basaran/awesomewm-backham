cc2410d696b72b86851500f5afd857895e59f8b8
anakha <basaran@protonmail.com>
2021-05-28 02:08:25 -0400

    initial

---
+++ init.lua
@@ -0,0 +1,17 @@
+-- allows automatically focusing back to the previous client
+-- on window close or minimize.
+
+function backham()
+    local s = awful.screen.focused()
+    local c = awful.client.focus.history.get(s, 0)
+    if c then
+        client.focus = c
+        c:raise()
+    end
+end
+
+client.connect_signal("property::minimized", backham)
+-- attach to minimized state
+
+client.connect_signal("unmanage", backham)
+-- attach to closed state
